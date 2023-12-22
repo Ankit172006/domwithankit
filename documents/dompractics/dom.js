27d3248d42c9068317492752737c410f55837fc2
Karan <karantha289@gmail.com>
2023-12-22 19:54:51 +0530

    add:> innerHTML textcontatant innertext setattribute in js

---
+++ documents/dompractics/dom.js
@@ -0,0 +1,49 @@
+let ankit=document.querySelector(".ankit")
+
+let hy=document.querySelector("#hy")
+document.body.style.backgroundColor="gray"
+ankit.style.display="flex"
+ankit.style.textDecoration="none"
+ankit.style.fontWeight="700"
+ankit.style.fontFamily="math"
+ankit.style.fontSize="50px"
+ankit.style.textShadow="5px 5px 5px #000"
+ankit.style.justifyContent="center"
+ankit.style.gap="10px"
+hy.style.textDecoration="none"
+
+console.log(ankit.innerHTML)
+console.log(ankit.textContent)
+console.log(ankit.innerText)
+
+hy.className="sky"
+console.log(ankit.className="hello")
+// console.log(ankit.innerHTML)
+
+let img=document.querySelector("img")
+
+let btn=document.querySelector("button")
+
+img.style.width="412px"
+btn.style.margin="10px"
+var i=1;
+document.body.style.display="flex"
+document.body.style.justifyContent="center"
+document.body.style.alignItems="center"
+document.body.style.flexDirection="column"
+function gls()
+{
+
+     if(i==1)
+     {
+        img.setAttribute("src" , "https://imgd.aeplcdn.com/1280x720/n/cw/ec/43658/mercedes-benz--exterior-0.jpeg")
+         i=2;
+         img.style.transition="1.5s"
+     }
+     else
+     {
+        img.setAttribute("src" , "https://imgd.aeplcdn.com/664x374/n/cw/ec/95489/maybach-gls-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80")
+        i=1;
+        img.style.transition=".9s" 
+     }
+}
