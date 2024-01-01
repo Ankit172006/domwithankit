3d91a0de1d91f38e6cc3e827b630f79be9177469
Karan <karantha289@gmail.com>
2024-01-01 08:29:30 +0530

    90% complete mini js project

--- documents/dompractics/projectno/prono.js
+++ documents/dompractics/projectno/prono.js
@@ -13,7 +13,7 @@ minus.addEventListener('click', function(){
         a--;
     }
    
-    a=(a>1)?"0"+a:a;
+    a=(a>1)?""+a:a;
     num.innerText=a;
  })
 
@@ -33,7 +33,7 @@ var a2=1;
          a2--;
      }
     
-     a2=(a2>0)?"0"+a2:a2;
+     a2=(a2>0)?""+a2:a2;
      num2.innerText=a2;
   })
 
@@ -54,7 +54,7 @@ var a3=1;
          a3--;
      }
     
-     a3=(a3>1)?"0"+a3:a3;
+     a3=(a3>1)?""+a3:a3;
      num3.innerText=a3;
   })
 //  let karan=document.querySelector(".karan")
@@ -75,6 +75,24 @@ let pess1=document.querySelector("#pess1")
 let lugg1=document.querySelector("#lugg1")
 let child1=document.querySelector("#child1")
 let dis1=document.querySelector("#dis1")
+
+//image function
+let img1=document.querySelector("#img1")
+let img2=document.querySelector("#img2")
+let bus=0;
+img1.addEventListener('click',()=>{
+bus=1;
+    img1.style.border="4px solid #000";
+    img2.style.border="none";
+    // console.log(bus)
+})
+img2.addEventListener('click',()=>{
+    bus=2;
+    img2.style.border="4px solid #000";
+    img1.style.border="none";
+    // console.log(bus)
+})
+
 sub.addEventListener('click', function(){
     var total=num.innerText;
 
@@ -83,8 +101,9 @@ sub.addEventListener('click', function(){
     var total3=num3.innerText;
     var amt3=total3*80;
     var kmtotal=document.querySelector("#number").value
-
- if(kmtotal<=100 )
+  if(bus==2)
+  {
+  if(kmtotal<=100 )
     {      
         var k1=12;
         var t1=250;
@@ -137,7 +156,72 @@ sub.addEventListener('click', function(){
     {
         alert("Sorry Enter valid Disstance")
     }
-    
+
+var maintotal=amt+amt2+amt3+amt4;
+ p.innerText="₹"+maintotal;
+ pess.innerText=amt;
+ lugg.innerText=amt2;
+ child.innerText=amt3;
+ dis.innerText=amt4;
+ totalamount.style.left="30%";
+ totalamount.style.transition="all 1.9s"
+}
+   else if(bus==1)
+   {
+    if(kmtotal<=100 )
+    {      
+        var k1=10;
+        var t1=210;
+        var tt1=130;
+        var ch1=80;     
+        var amt4=kmtotal*10;
+        var amt=total*210;
+        var amt2=total2*130;
+
+
+        pess1.innerText=t1+"*";
+        lugg1.innerText=tt1+"*";
+        child1.innerText=ch1+"*";
+        dis1.innerText=k1+"*";
+
+
+    }
+    else if(kmtotal<=200 )
+    {
+        var k1=18;
+        var t1=300;
+        var tt1=170;
+        var ch1=80;  
+
+        var amt4=kmtotal*18;
+        var amt=  total*300;
+        var amt2=total2*170;
+        
+        pess1.innerText=t1+"*";
+        lugg1.innerText=tt1+"*";
+        child1.innerText=ch1+"*";
+        dis1.innerText=k1+"*";
+    }
+    else if(kmtotal<=300 )
+    {
+        var k1=26;
+        var t1=440;
+        var tt1=210;
+        var ch1=80; 
+        var amt4=kmtotal*26;
+        var amt=  total*440;
+        var amt2=total2*210;
+        pess1.innerText=t1+"*";
+        lugg1.innerText=tt1+"*";
+        child1.innerText=ch1+"*";
+        dis1.innerText=k1+"*";
+    }
+
+    else
+    {
+        alert("Sorry Enter valid Disstance")
+    }
+   
     var maintotal=amt+amt2+amt3+amt4;
      p.innerText="₹"+maintotal;
      pess.innerText=amt;
@@ -145,7 +229,12 @@ sub.addEventListener('click', function(){
      child.innerText=amt3;
      dis.innerText=amt4;
      totalamount.style.left="30%";
-     totalamount.style.transition="all .9s"
+     totalamount.style.transition="all 1.9s"
+}
+else
+{
+    alert("choose Your Bus")
+}
 })
 
 // var kmtotal=document.querySelector("#number").value
@@ -180,17 +269,29 @@ sub.addEventListener('click', function(){
 //         alert("Enter under 300km")
 //     }
 // })
-
-
-
-
-
-
-
-
-
 let cross=document.querySelector(".cross")
 cross.addEventListener('click',()=>{
     totalamount.style.left="-100%";
 })
 
+
+// let in1=document.querySelector("#redio1")
+// let in2=document.querySelector("#redio2")
+// console.log(in1)
+// console.log(in2)
+
+
+//  let get=document.
+//    seats35=35;
+//    console.log(se)
+
+//    if(se==35)
+//    {
+//     console.log("True")
+//    }
+//    else
+//    {
+//     console.log("false")
+//    }
+//     console.log(seats35)
+    // console.log(seats30)
